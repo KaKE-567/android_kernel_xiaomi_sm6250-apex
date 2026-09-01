49018ba8c5ce2276ff03cd330ca2c35ff7fb52ee
KaKE <botwheel972@gmail.com>
2026-09-01 17:26:41 +0000

    arm64: alternative: Restore 664 label in __ALTERNATIVE_CFG

--- arch/arm64/include/asm/alternative.h
+++ arch/arm64/include/asm/alternative.h
@@ -71,6 +71,7 @@ void apply_alternatives(void *start, size_t length);
 	".pushsection .altinstr_replacement, \"a\"\n"			\
 	"663:\n\t"							\
 	newinstr "\n"							\
+	"664:\n\t"							\
 	".org	. - (664b-663b) + (662b-661b)\n\t"			\
 	".org	. - (662b-661b) + (664b-663b)\n\t"			\
 	".popsection\n"							\
